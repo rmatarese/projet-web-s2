945bc24d49f47a3b0cc56c1f516b25ea24defbff
Sash-cmd <sasha.le-roux-zielinski@student.junia.com>
2023-04-22 19:16:16 +0200

    AH
    j'ai un peu oublié la 2eme partie du machin

---
+++ ajout.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+
+// Vérifier si les données du formulaire ont été soumises
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Se connecter à la base de données MySQL
+    $host = "localhost";
+    $user = "root";
+    $password = "root";
+    $dbname = "alabar";
+
+    $conn = mysqli_connect($host, $user, $password, $dbname);
+
+    // Vérifier la connexion
+    if (!$conn) {
+        die("La connexion à la base de données a échoué: " . mysqli_connect_error());
+    }
+    // Récupérer les données du formulaire
+    $bar_id = $_POST['bar-id'];
+    $bar_name = $_POST['bar-name'];
+    $bar_city = $_POST['bar-city'];
+    $bar_address = $_POST['bar-address'];
+
+    // Créer une requête SQL pour insérer les données du formulaire dans la base de données
+    $sql = "INSERT INTO bars (IdBar, NomBar, AdresseBar, Villebar) VALUES ('$bar_id', '$bar_name','$bar_address','$bar_city')";
+
+    // Exécuter la requête SQL
+    if (mysqli_query($conn, $sql)) {
+        echo "Les informations du bar ont été enregistrées avec succès.Merci beaucoup pour votre participation !";
+    } else {
+        echo "Erreur: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+    // Fermer la connexion à la base de données
+    mysqli_close($conn);
+
+} else {
+    header("Location: ajout_bar.php");
+}
+?>
+<DOCTYPE html>
+<html>
+<a href="page_client.php">Retourner à la page client</a>
+</html>
--- page_client.php
+++ page_client.php
@@ -1,4 +1,11 @@
+
+<?PHP
 session_start();
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$database = "alabar";
+
 if(!isset($_SESSION['username'])){
     header("Location: login.php");
 }
@@ -6,3 +13,18 @@ $conn= new PDO("mysql:host=$servername;dbname=$database", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $statement = $conn->prepare("SELECT * FROM client WHERE username = :username");
 $statement->bindParam(':username', $_SESSION['username']);
+
+?>
+<DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Page client</title>
+</head>
+<body>
+    <h1>Page client</h1>
+    <p>Bienvenue sur votre page client. Vous pouvez ajouter un bar à notre liste.</p>
+    <a href="ajout_bar.php">Ajouter un bar</a>
+    <a href="deconnexion.php">Se déconnecter</a>
+</body>
+</html>
